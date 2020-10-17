b460070eabc3b15fec4893c4f137a269cf99b539
Mihir Srivastava <msrivastava574@gmail.com>
2020-10-17 23:38:14 +0530

    :lipstick: added bootstrap css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoCallModule } from './video-call/video-call.module';
 import { BillingModule } from './billing/billing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BillingModule, VideoCallModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BillingModule,
+    VideoCallModule,
+    NgbModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
